9bbdcb17db019a512cc68f40bffa66ec1cfa0e9b
bonily <bonilya@gmail.com>
2020-05-25 17:24:18 +0300

    Товары

--- source/js/main.js
+++ source/js/main.js
@@ -2,7 +2,12 @@
 
 const screenWidth = screen.width;
 const consultationButton = document.querySelector('.consultation__button');
+const aboutCompanyDescription = document.querySelector('.about-company__container')
 
 if (screenWidth < 767) {
   consultationButton.textContent = `бесплатная консультация`
+}
+
+if (screenWidth < 1023) {
+  aboutCompanyDescription.textContent = `..`
 }

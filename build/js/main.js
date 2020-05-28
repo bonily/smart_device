d2236f5f1d7cca9d71cd74a31ab74a4a0b4dc5cc
bonily <bonilya@gmail.com>
2020-05-28 13:33:16 +0300

    Правки до прокрутки

--- build/js/main.js
+++ build/js/main.js
@@ -2,6 +2,7 @@
 
 (function () {
 
+  const popapPlace = document.querySelector('main');
   const tabletWidth = '(max-width: 1023px)';
   const mobileWidth = '(max-width: 767px)';
   const consultationButton = document.querySelector('.consultation-button');
@@ -9,6 +10,11 @@
   const sliceBlocks = document.querySelectorAll('.about-company p');
   const hiddenBlocks = document.querySelectorAll('.page-footer__smart-block');
   const openButtons = document.querySelectorAll('.page-footer__smart-block button');
+  const callButton = document.querySelector('.call-button');
+  const popapMessageTemplate = document.querySelector('.popap__template');
+  const scrollAdvantagesButton = document.querySelector('.main-block__scroll');
+  let popapMessage;
+
 
   if (window.matchMedia(mobileWidth).matches) {
     consultationButton.textContent = 'бесплатная консультация';
@@ -34,10 +40,13 @@
     });
   }
 
-  const phone = new window.IMask(
-      phonePlace, {
-        mask: '+{7}(000)000-00-00'
-      });
+
+  function createPhoneMask(element) {
+    return new window.IMask(
+        element, {
+          mask: '+{7}(000)000-00-00'
+        });
+  }
 
   phonePlace.addEventListener('focus', function () {
     phonePlace.value = '+7(';
@@ -48,4 +57,75 @@
       phonePlace.value = '';
     }
   });
+
+  callButton.addEventListener('click', function () {
+    popapMessage = popapMessageTemplate.cloneNode(true);
+    popapMessage.classList.remove('popap__template');
+
+    const closePopapButton = popapMessage.querySelector('.popap__close-button');
+    const form = popapMessage.querySelector('.popap__form');
+    const popapNamePlace = popapMessage.querySelector('#popap-name');
+    const popapPhonePlace = popapMessage.querySelector('#popap-phone');
+    const popapTextMessage = popapMessage.querySelector('#popap-comment');
+
+    createPhoneMask(popapPhonePlace);
+    popapNamePlace.focus();
+    popapNamePlace.value = window.localStorage.getItem('name') || '';
+    popapPhonePlace.value = window.localStorage.getItem('number') || '';
+    popapTextMessage.value = window.localStorage.getItem('message') || '';
+    popapPlace.appendChild(popapMessage);
+
+    form.addEventListener('submit', function (evt) {
+      evt.preventDefault();
+      window.localStorage.setItem('name', popapNamePlace.value);
+      window.localStorage.setItem('number', popapPhonePlace.value);
+      window.localStorage.setItem('message', popapTextMessage.value);
+      popapPlace.removeChild(popapMessage);
+      document.removeEventListener('keydown', onPopapKeydown);
+      document.removeEventListener('click', onOverlayClick);
+    });
+
+    closePopapButton.addEventListener('click', function () {
+      popapPlace.removeChild(popapMessage);
+    });
+
+    document.addEventListener('keydown', onPopapKeydown);
+
+    document.addEventListener('click', onOverlayClick);
+  });
+
+
+  function onPopapKeydown(evt) {
+    if (evt.key === 'Escape' || evt.key === 'Esc') {
+      popapPlace.removeChild(popapMessage);
+      document.removeEventListener('keydown', onPopapKeydown);
+      document.removeEventListener('click', onOverlayClick);
+    }
+  }
+
+  function onOverlayClick(evt) {
+    if (evt.target.className === 'popap') {
+      popapPlace.removeChild(popapMessage);
+      document.removeEventListener('click', onOverlayClick);
+      document.removeEventListener('keydown', onPopapKeydown);
+    }
+  }
+  scrollAdvantagesButton.addEventListener('click', function (evt) {
+    evt.preventDefault();
+    smoothScroll(scrollAdvantagesButton);
+  });
+
+  consultationButton.addEventListener('click', function (evt) {
+    evt.preventDefault();
+    smoothScroll(consultationButton);
+  });
+
+  function smoothScroll(element) {
+    const targetElementId = element.getAttribute('href');
+    const targetElement = document.querySelector(targetElementId);
+
+    targetElement.scrollIntoView({behavior: "smooth"});
+  }
+
+  createPhoneMask(phonePlace);
 })();
